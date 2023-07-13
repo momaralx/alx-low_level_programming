e37e86c3c7298f75de4e1838eb33547a20dd2ea5
unknown989 <unknown989@proton.me>
2023-07-13 07:57:14 +0100

    Quote

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,10 @@
+#include <unistd.h>
+#include <stdio.h>
+#include <string.h>
+int main() {
+    const char* quote = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
+    fflush(stdout);
+    write(STDOUT_FILENO, "Hello", strlen(quote));
+
+    return 0;
+}
