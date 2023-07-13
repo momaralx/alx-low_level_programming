064a66ca40e2b76cf6cfc6bebac5a92642037356
unknown989 <unknown989@proton.me>
2023-07-13 07:32:01 +0100

    fixed puts(betty style)

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,12 @@
 #include <stdio.h>
 
-int main(void){
-  puts("\"Programming is like building a multilingual puzzle");
+/**
+ * main - Entry point
+ * Return: 0 always
+*/
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
 
-  return 0;
+return (0);
 }
