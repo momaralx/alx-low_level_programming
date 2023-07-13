d21d8605aaf94e26fc5d9f84094d2e644373306f
unknown989 <unknown989@proton.me>
2023-07-13 07:58:46 +0100

    Fixed Quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,9 +2,8 @@
 #include <stdio.h>
 #include <string.h>
 int main() {
-    const char* quote = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
+    const char* quote = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
     fflush(stdout);
     write(STDOUT_FILENO, quote, strlen(quote));
-
-    return 0;
+    return 1;
 }
