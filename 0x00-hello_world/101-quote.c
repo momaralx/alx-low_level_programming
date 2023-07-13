76808a7996b091f660d43028c477e6845cde8b71
unknown989 <unknown989@proton.me>
2023-07-13 07:57:58 +0100

    Fixed Quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -4,7 +4,7 @@
 int main() {
     const char* quote = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
     fflush(stdout);
-    write(STDOUT_FILENO, "Hello", strlen(quote));
+    write(STDOUT_FILENO, quote, strlen(quote));
 
     return 0;
 }
