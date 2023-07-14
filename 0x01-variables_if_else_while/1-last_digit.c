9d6c4cc5f1ac5a83eb94aba371cd17f297412d32
unknown989 <unknown989@proton.me>
2023-07-14 10:55:32 +0100

    Fixed(betty) Last digit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -9,28 +9,31 @@
 */
 int main(void)
 {
-    int n, sign, last_digit_of_n;
-    char f[100];
-    char buffer[100];
+int n, sign, last_digit_of_n;
+char f[100];
+char buffer[100];
 
-    srand(time(0));
-    n = rand() - RAND_MAX / 2;
-    
-    sign = abs(n) / n;
-    sprintf(buffer, "%d", n);
-    
-    last_digit_of_n = sign * atoi(&buffer[strlen(buffer)- 1]);
-    if ( last_digit_of_n > 5 )
-    {
-      strcpy(f, "and is greater than 5");
-    }
-    else if ( last_digit_of_n == 0 ){
-      strcpy(f, "and is 0");
-    }else{
-      strcpy(f, "and is less than 6 and not 0");
-    }
+srand(time(0));
+n = rand() - RAND_MAX / 2;
 
-    printf("Last digit of %d is %d %s\n", n, last_digit_of_n, f );
+sign = abs(n) / n;
+sprintf(buffer, "%d", n);
 
-    return (0);
+last_digit_of_n = sign *atoi(&buffer[strlen(buffer) - 1]);
+if (last_digit_of_n > 5)
+{
+strcpy(f, "and is greater than 5");
+}
+else if (last_digit_of_n == 0)
+{
+strcpy(f, "and is 0");
+}
+else
+{
+strcpy(f, "and is less than 6 and not 0");
+}
+
+printf("Last digit of %d is %d %s\n", n, last_digit_of_n, f);
+
+return (0);
 }
