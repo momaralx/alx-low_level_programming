12aaa92723510c16c4e01930b8e764633fe75b74
unknown989 <unknown989@proton.me>
2023-07-14 11:13:46 +0100

    print base16

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: 0 always
+*/
+int main(void)
+{
+int i;
+
+for (i = 48; i <= 57; i++)
+{
+putchar(i);
+}
+for (i = 97; i <= 102; i++)
+{
+putchar(i);
+}
+putchar('\n');
+
+return (0);
+}
