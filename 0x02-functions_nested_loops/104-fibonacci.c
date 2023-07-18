78efe0d78269c4dbf934733158acd6e67007a9e2
unknown989 <unknown989@proton.me>
2023-07-18 23:33:23 +0100

    fibonacci

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: 0 always
+ */
+int main(void)
+{
+long int n1;
+long int n2;
+long int nth;
+int count;
+n1 = 1;
+n2 = 2;
+count = 0;
+while (count < 98)
+{
+if (count == 97)
+{
+printf("%lu", n1);
+}
+else
+{
+printf("%lu, ", n1);
+}
+nth = n1 + n2;
+n1 = n2;
+n2 = nth;
+count++;
+}
+printf("\n");
+return (0);
+}
