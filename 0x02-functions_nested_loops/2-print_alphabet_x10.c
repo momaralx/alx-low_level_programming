9228a843649273e7e5771c4eda9e9d0aedeb3713
unknown989 <unknown989@proton.me>
2023-07-18 21:02:41 +0100

    alphabet x10

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - print alphabet x10
+ *
+ * Return: nothing
+ */
+void print_alphabet_x10(void){
+int j;
+for (j = 0; j < 10;j++){
+int i;
+for (i = 97; i <= 122; i++)
+{
+_putchar(i);
+}
+_putchar('\n');
+}
+}
