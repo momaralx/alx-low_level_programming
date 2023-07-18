b7b11eaa91ffaf4e6c0b0b42b1e36f3b9eef15e9
unknown989 <unknown989@proton.me>
2023-07-18 22:03:16 +0100

    fixed 24 hours

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,19 +1,23 @@
 #include "main.h"
+/**
+ * jack_bauer - print every minute of the day of Jack Bauer
+ * Return: nothing
+ */
 
- void jack_bauer(void)
+void jack_bauer(void)
 {
-  int min;
-  int hr;
-  for (hr = 0; hr <= 23; hr++)
-  {
-    for (min = 0; min <= 59; min++)
-    {
-      _putchar((int)hr/10);
-      _putchar((int)hr%10);
-      _putchar(':');
-      _putchar((int)min/10);
-      _putchar((int)hr%10);
-      _putchar('\n');
-    }
-  }
+int min;
+int hr;
+for (hr = 0; hr <= 23; hr++)
+{
+for (min = 0; min <= 59; min++)
+{
+_putchar((int)hr / 10 + '0');
+_putchar((int)hr % 10 + '0');
+_putchar(':');
+_putchar((int)min / 10 + '0');
+_putchar((int)hr % 10 + '0');
+_putchar('\n');
+}
+}
 }
