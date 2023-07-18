c8331ed69b487dbab2d93e4d663f60eadbd04f75
unknown989 <unknown989@proton.me>
2023-07-18 21:20:54 +0100

    fixed print sign

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -11,11 +11,15 @@ int print_sign(int n)
 {
 if (n == 0)
 {
-return '0';
+_putchar('0');
+return 0;
 }
 else if (n > 0)
 {
-return '+';
+_putchar('1');
+return 1;
 }
-return '-';
+_putchar('-');
+_putchar('1');
+return -1;
 }
