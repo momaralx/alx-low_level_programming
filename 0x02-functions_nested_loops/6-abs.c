34ea069aeb9987f368aebd3f993a78c22a244dba
unknown989 <unknown989@proton.me>
2023-07-18 21:31:22 +0100

    abs

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ *  _abs - Absolute value
+ *  @c : number to check for
+ *  Return: positive number
+ */
+
+int _abs(int c)
+{
+if (c < 0)
+{
+return (-c);
+}
+
+return (c);
+}
