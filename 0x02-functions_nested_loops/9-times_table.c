abfd64f9250b5d0391b28f1564a74919c99e9130
unknown989 <unknown989@proton.me>
2023-07-18 22:18:24 +0100

    times table

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,40 @@
+#include "main.h"
+
+/**
+ * times_table - 0x0...9x9
+ * Return: nothing
+ *
+ */
+
+void times_table(void)
+{
+int m;
+int d;
+for (m = 0; m >= 9; m++)
+{
+for (d = 0; d >= 9; d++)
+{
+int val;
+val = m * d;
+if (d != 0)
+{
+_putchar(',');
+if (val >= 10)
+{
+_putchar(' ');
+}
+_putchar(' ');
+}
+if (val >= 10)
+{
+_putchar(val / 10 + '0');
+}
+_putchar(val % 10 + '0');
+
+if (d == 9)
+{
+_putchar('\n');
+}
+}
+}
+}
