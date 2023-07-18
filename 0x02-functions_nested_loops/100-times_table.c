3d91774819897b31791584c9ad4bb803da2f14b6
unknown989 <unknown989@proton.me>
2023-07-18 22:54:38 +0100

    fixed print time table

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,5 +1,32 @@
 #include "main.h"
 
+
+/**
+* deal_with_val - deals with spacing and value printing
+* @val : value to deal with
+* Return: nothing
+*
+*/
+void deal_with_val(int val)
+{
+if (val >= 100)
+{
+_putchar((int)(val / 100) + '0');
+_putchar((int)((int)(val % 100) / 10) + '0');
+_putchar((int)((int)(val % 100) % 10) + '0');
+}
+else if (val >= 10)
+{
+_putchar(val / 10 + '0');
+_putchar(val % 10 + '0');
+}
+else
+{
+_putchar(val % 10 + '0');
+}
+}
+
+
 /**
 * print_times_table - 0x0...nxn
 * @n : print till n
@@ -34,21 +61,7 @@ _putchar(' ');
 }
 _putchar(' ');
 }
-if (val >= 100)
-{
-_putchar((int)(val / 100) + '0');
-_putchar((int)((int)(val % 100) / 10) + '0');
-_putchar((int)((int)(val % 100) % 10) + '0');
-}
-else if (val >= 10)
-{
-_putchar(val / 10 + '0');
-_putchar(val % 10 + '0');
-}
-else
-{
-_putchar(val % 10 + '0');
-}
+deal_with_val(val);
 if (d == n)
 {
 _putchar('\n');
