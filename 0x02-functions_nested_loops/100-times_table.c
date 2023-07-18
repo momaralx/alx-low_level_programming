9338e1626abf85804dc5d4b6b579461fd2bdb97c
unknown989 <unknown989@proton.me>
2023-07-18 22:38:31 +0100

    print time table

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,55 @@
+#include "main.h"
+
+/**
+ * print_times_table - 0x0...nxn
+ * @n : print till n
+ * Return: nothing
+ *
+ */
+
+void print_times_table(int n)
+{
+int m;
+int d;
+for (m = 0; m <= n; m++)
+{
+for (d = 0; d <= n; d++)
+{
+int val;
+val = m * d;
+if (d != 0)
+{
+_putchar(',');
+if (val < 10)
+{
+_putchar(' ');
+}
+if(val < 100)
+{
+_putchar(' ');
+}
+_putchar(' ');
+}
+if (val >= 10)
+{
+_putchar(val / 10 + '0');
+_putchar(val % 10 + '0');
+}
+else if (val >= 100)
+{
+_putchar(val / 100 + '0');
+_putchar((val % 100)/10 + '0');
+_putchar((val % 100)%10 + '0');
+}
+else
+{
+_putchar(val % 10 + '0');
+}
+
+if (d == 9)
+{
+_putchar('\n');
+}
+}
+}
+}
